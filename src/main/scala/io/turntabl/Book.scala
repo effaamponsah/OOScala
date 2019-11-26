99ec55eeb3d3c051727bd7e3c2103d874cd2994b
Dennis Effa <kamponsah.amponsah@gmail.com>
2019-11-26 16:06:36 +0000

    functional init

---
+++ src/main/scala/io/turntabl/Book.scala
@@ -0,0 +1,3 @@
+package io.turntabl
+
+case class Book(authorSurnamme:String, title:String, publishedYr:Int, category:String)
--- src/main/scala/io/turntabl/scalagettingstarted/MyApp.scala
+++ src/main/scala/io/turntabl/scalagettingstarted/MyApp.scala
@@ -1,28 +1,11 @@
 package io.turntabl.scalagettingstarted
-
+import io.turntabl.Book
 
 object MyApp extends App {
-    val myself = ("Dennis", "python")
-    val name, favLang = myself
 
+    val books = List(
+        Book("James","Hammer in the head",2019,"Fiction"),
+        Book("Kwame","Hammer in the head",2018,"Fiction")
 
-    val countries = Map("Britian" -> "London",
-        "Ghana" -> "Accra",
-        "Togo" -> "Lome") + ("Ethiopia"->"Addis Ababa", "Egypt"->"Cairo")
-
-    print(countries)
-    countries.getOrElse("Norway", "unknown")
-
-    val friends = Set("James", "John", "Hannah")+("Samuel","Samuel")
-
-    println(friends.count(_ == "Samuel"))
-
-    val names =List("Christiana","Francis","Bill")
-    names foreach(print)
-
-    names filter(name => name.length > 6) foreach(print)
-
-    print(names count(_.length > 6))
-
-
+    )
 }
