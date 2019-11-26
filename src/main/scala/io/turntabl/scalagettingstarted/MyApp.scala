e6961c43501d8aeb76e13f49f04919034063422b
Dennis Effa <kamponsah.amponsah@gmail.com>
2019-11-26 16:24:23 +0000

    added a list of books together with publish method

--- src/main/scala/io/turntabl/scalagettingstarted/MyApp.scala
+++ src/main/scala/io/turntabl/scalagettingstarted/MyApp.scala
@@ -5,7 +5,14 @@ object MyApp extends App {
 
     val books = List(
         Book("James","Hammer in the head",2019,"Fiction"),
-        Book("Kwame","Hammer in the head",2018,"Fiction")
-
+        Book("Kwame","Hello from other side",2018,"Drama"),
+        Book("Akosua","Ananse",1957,"Fiction"),
+        Book("Theon","Okay Google",2017,"Adventure"),
+        Book("Christy","Thanks",2019,"Thriller"),
+        Book("Bill","Decoding Bill Gates",2015,"Fiction")
     )
+
+    val publish=(year:Int, listBooks:List[Book]) => listBooks filter(book => book.publishedYr == year) foreach(print)
+
+    publish(2019, books)
 }
