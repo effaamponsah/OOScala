3c3ee082d863e88ed6415523efb487b3fcb71c1e
Dennis Effa <kamponsah.amponsah@gmail.com>
2019-11-27 10:01:43 +0000

    done with questions 3 and 4

--- src/main/scala/io/turntabl/scalagettingstarted/MyApp.scala
+++ src/main/scala/io/turntabl/scalagettingstarted/MyApp.scala
@@ -9,10 +9,20 @@ object MyApp extends App {
         Book("Akosua","Ananse",1957,"Fiction"),
         Book("Theon","Okay Google",2017,"Adventure"),
         Book("Christy","Thanks",2019,"Thriller"),
-        Book("Bill","Decoding Bill Gates",2015,"Fiction")
+        Book("Bill","Decoding Bill Gates",2015,"Fiction"),
+        Book("Steve","Human",1995,"Fiction"),
+        Book("Hannah","Human brain",20112,"Drama"),
+        Book("Nkrumah","Ananse",2010,"Fiction"),
+        Book("Gates","Okay Google",2000,"Adventure"),
+        Book("Jeff","Thanks",1880,"Thriller"),
+        Book("Andrew","Decoding Bill Gates",1998,"Comedy")
     )
 
-    val publish=(year:Int, listBooks:List[Book]) => listBooks filter(book => book.publishedYr == year) foreach(print)
+//    3
+    val publish=(year:Int, listBooks:List[Book]) => listBooks filter(book => book.publishedYr == year) foreach(println)
 
-    publish(2019, books)
+//    publish(2019, books)
+//    4
+    val after99=(bookList:List[Book])=> bookList filter(book => book.publishedYr > 1999) map(book => book.title) foreach(println)
+    after99(books)
 }
