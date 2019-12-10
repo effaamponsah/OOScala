d8b5d44a5c0a7d37f880f2eee229a5566c76847f
Dennis Effa <kamponsah.amponsah@gmail.com>
2019-12-10 14:25:02 +0000

    clearing space

--- src/main/scala/io/turntabl/Book.scala
+++ src/main/scala/io/turntabl/Book.scala
@@ -1,3 +1,5 @@
 package io.turntabl
 
-case class Book(authorSurnamme:String, title:String, publishedYr:Int, category:String)
+case class Book(authorSurnamme:String, title:String, publishedYr:Int, category:String) {
+  override def equals(that: Any): Boolean = true
+}
